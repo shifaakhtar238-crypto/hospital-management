6591af5ed500a687b52c52fc0bb2ff5946f9524e
Shifa Akhtar <shifaakhtar238@gmail.com>
2026-04-18 22:12:34 +0530

    Fix supabase client config

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -1,6 +1,8 @@
-import { createClient } from '@supabase/supabase-js'
+const { createClient } = require("@supabase/supabase-js");
 
-const supabaseUrl = "https://izzeeuharkhanpzgyuzn.supabase.co"
-const supabaseKey = "sb_publishable_7oXFJEZQ6_kHaCTVsHNtKw_4elV1miN"
+const supabaseUrl = "https://izzeeuharkhanpzgyuzn.supabase.co";
+const supabaseKey = "sb_publishable_7oXFJEZQ6_kHaCTVsHNtKw_4elV1miN";
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+module.exports = supabase;
